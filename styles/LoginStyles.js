692d80a9f69b4c8d5501634eabf892754afdefa7
Gustavo Correa <gustaalves32@gmail.com>
2024-10-20 22:03:18 -0300

    Update LoginStyles.js
    esqueci de mudar a cor kkkkkkk faz o L kaua

--- styles/LoginStyles.js
+++ styles/LoginStyles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: rgb(18, 18, 18); 
+  background-color: rgb(10, 10, 10); 
   justify-content: center;
   align-items: center;
   padding: 20px;
@@ -31,17 +31,16 @@ export const Input = styled.TextInput`
 export const Botao = styled.TouchableOpacity`
   width: 100%;
   height: 50px;
-  background-color: rgb(255, 50, 50); 
+  background-color: rgb(255, 255, 255); 
   justify-content: center;
   align-items: center;
   border-radius: 10px;
   margin-bottom: 15px;
   elevation: 3;
-  box-shadow: 0 4px 10px rgba(255, 50, 50, 0.6); 
 `;
 
 export const BotaoTexto = styled.Text`
-  color: rgb(255, 255, 255); 
+  color: rgb(0, 0, 0); 
   font-size: 20px;
   font-weight: bold;
   letter-spacing: 1px;
