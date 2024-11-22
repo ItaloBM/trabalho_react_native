b0bc260b1ccf5d6157f6856a73b44a708bb8048a
Italo <italobm2011@gmail.com>
2024-11-21 23:40:07 -0300

    Chat funcionando

--- databases/Firebase.js
+++ databases/Firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Importar AsyncStorage
 
 const firebaseConfig = {
   apiKey: "AIzaSyCYh4g1BnyWzMuvbtbC5Xs_6lnV9Bv76e4",
@@ -11,11 +12,22 @@ const firebaseConfig = {
   appId: "1:741240661644:web:79a1f4ad7e024504c6fda9",
 };
 
-
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-
+// Para autenticação com persistência usando AsyncStorage
 const auth = getAuth(app);
+
+// Configurar persistência
+auth.setPersistence(AsyncStorage)
+  .then(() => {
+    // Agora o estado de autenticação será persistido
+    console.log('Persistência configurada!');
+  })
+  .catch((error) => {
+    console.error('Erro ao configurar persistência:', error);
+  });
+
 const db = getFirestore(app);
 
 export { auth, db };
